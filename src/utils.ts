71c3baf8e58aedb90ea8514ce43c8019d6558fa2
Marko Jakic <jakicmeister@gmail.com>
2025-02-05 13:57:52 +0100

    reverse, lang, links

--- src/utils.ts
+++ src/utils.ts
@@ -52,3 +52,82 @@ export const toBreadcrumbText = (str: string) => {
       .replace(/-/g, " ")
   );
 };
+
+export const pathMapperEn: Record<string, string> = {
+  "/about": "/ueber-uns",
+  "/careers": "/karriere",
+  "/contact": "/kontakt",
+  "/nearshoring": "/nearshoring-de",
+  "/software-development": "/softwareentwicklung",
+  "/it-strategy-planning": "/it-strategie-planung",
+  "/custom-software-development": "/individuelle-softwareentwicklung",
+  "/web-development": "/webentwicklung",
+  "/mobile-app-development": "/mobile-app-entwicklung",
+  "/cloud-development": "/cloud-entwicklung",
+  "/api-development": "/api-entwicklung",
+  "/technology-assessment": "/technologiebewertung",
+  "/infrastructure-consulting": "/infrastrukturberatung",
+  "/cybersecurity-consulting": "/cybersecurity-beratung",
+  "/digital-transformation-consulting": "/digitale-transformationsberatung",
+  "/compliance-risk-management": "/compliance-risikomanagement",
+  "/dedicated-development-teams": "/dedizierte-entwicklungsteams",
+  "/project-based-nearshoring": "/projektbasiertes-nearshoring",
+  "/staff-augmentation": "/personal-augmentation",
+  "/quality-assurance": "/qualitaetsmanagement",
+  "/maintenance-support": "/wartung-support",
+  "/machine-learning-solutions": "/machine-learning-loesungen",
+  "/natural-language-processing-nlp": "/natuerliche-sprachverarbeitung-nlp",
+  "/computer-vision-applications": "/computer-vision-anwendungen",
+  "/predictive-analytics": "/praediktive-analytik",
+  "/retrieval-augmented-generation-rag":
+    "/retrieval-augmented-generation-rag-de",
+  "/ai-powered-chatbots": "/ki-gestuetzte-chatbots",
+  "/insights/api-gateway-for-postgres-for-crud-and-vector-search":
+    "/insights/api-gateway-fuer-postgres-fuer-crud-und-vektorsuche",
+  "/insights/how-to-sign-a-contract-with-an-it-outsourcing-agency":
+    "/insights/so-schliessen-sie-einen-vertrag-mit-einer-it-outsourcing-agentur-ab",
+  "/insights/big-companies-that-were-built-using-outsources-developers":
+    "/insights/grosse-unternehmen-die-mithilfe-ausgelagerter-entwickler-aufgebaut-wurden",
+  "/insights/team-as-a-service-outsourcing-model":
+    "/insights/team-as-a-service-outsourcing-modell",
+};
+
+export const pathMapperDe: Record<string, string> = {
+  "/ueber-uns": "/about",
+  "/karriere": "/careers",
+  "/kontakt": "/contact",
+  "/nearshoring-de": "/nearshoring",
+  "/softwareentwicklung": "/software-development",
+  "/it-strategie-planung": "/it-strategy-planning",
+  "/individuelle-softwareentwicklung": "/custom-software-development",
+  "/webentwicklung": "/web-development",
+  "/mobile-app-entwicklung": "/mobile-app-development",
+  "/cloud-entwicklung": "/cloud-development",
+  "/api-entwicklung": "/api-development",
+  "/technologiebewertung": "/technology-assessment",
+  "/infrastrukturberatung": "/infrastructure-consulting",
+  "/cybersecurity-beratung": "/cybersecurity-consulting",
+  "/digitale-transformationsberatung": "/digital-transformation-consulting",
+  "/compliance-risikomanagement": "/compliance-risk-management",
+  "/dedizierte-entwicklungsteams": "/dedicated-development-teams",
+  "/projektbasiertes-nearshoring": "/project-based-nearshoring",
+  "/personal-augmentation": "/staff-augmentation",
+  "/qualitaetsmanagement": "/quality-assurance",
+  "/wartung-support": "/maintenance-support",
+  "/machine-learning-loesungen": "/machine-learning-solutions",
+  "/natuerliche-sprachverarbeitung-nlp": "/natural-language-processing-nlp",
+  "/computer-vision-anwendungen": "/computer-vision-applications",
+  "/praediktive-analytik": "/predictive-analytics",
+  "/ki-gestuetzte-chatbots": "/ai-powered-chatbots",
+  "/retrieval-augmented-generation-rag-de":
+    "/retrieval-augmented-generation-rag",
+  "/ai-powered-chatbots": "/ai-powered-chatbots",
+  "/insights/api-gateway-fuer-postgres-fuer-crud-und-vektorsuche":
+    "/insights/api-gateway-for-postgres-for-crud-and-vector-search",
+  "/insights/so-schliessen-sie-einen-vertrag-mit-einer-it-outsourcing-agentur-ab":
+    "/insights/how-to-sign-a-contract-with-an-it-outsourcing-agency",
+  "/insights/grosse-unternehmen-die-mithilfe-ausgelagerter-entwickler-aufgebaut-wurden":
+    "/insights/big-companies-that-were-built-using-outsources-developers",
+  "/insights/team-as-a-service-outsourcing-modell":
+    "/insights/team-as-a-service-outsourcing-model",
+};
